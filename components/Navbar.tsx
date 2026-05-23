c39a5b44d2dd1c3165c1a006b2fa79625d67836f
ADIB BAGHDADI <edip.baghdady99@gmail.com>
2026-05-23 21:01:27 +0300

    change login link to account

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
           ))}
 
           <Link href="/login" className="nav-link">
-            تسجيل الدخول
+             حسابي
           </Link>
         </nav>
 
